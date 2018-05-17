1157c074ada8e66e4fb3b6b0817e41553a3ada6e
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-05-16 22:17:38 -0400

    remove mkdirp lib

--- src/services/lowdbStorage.service.ts
+++ src/services/lowdbStorage.service.ts
@@ -1,7 +1,6 @@
 import * as fs from 'fs';
 import * as lowdb from 'lowdb';
 import * as FileSync from 'lowdb/adapters/FileSync';
-import * as mkdirp from 'mkdirp';
 import * as path from 'path';
 
 import { StorageService } from 'jslib/abstractions/storage.service';
@@ -20,7 +19,7 @@ export class LowdbStorageService implements StorageService {
             p = path.join(process.env.HOME, '.config', appDirName);
         }
         if (!fs.existsSync(p)) {
-            mkdirp.sync(p, 755);
+            this.mkdirpSync(p, 755);
         }
         p = path.join(p, 'data.json');
 
@@ -41,5 +40,17 @@ export class LowdbStorageService implements StorageService {
     remove(key: string): Promise<any> {
         this.db.unset(key).write();
         return Promise.resolve();
+    }
+
+    private mkdirpSync(targetDir: string, mode = 755, relative = false) {
+        const initialDir = path.isAbsolute(targetDir) ? path.sep : '';
+        const baseDir = relative ? __dirname : '.';
+        targetDir.split(path.sep).reduce((parentDir, childDir) => {
+            const dir = path.resolve(baseDir, parentDir, childDir);
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir, mode);
+            }
+            return dir;
+        }, initialDir);
     }
 }
