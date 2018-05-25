85211e05abad3ada18d793ac0d5505eee0f21d8a
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-05-25 14:51:30 -0400

    snap build script

---
+++ scripts/snap-build.ps1
@@ -0,0 +1,25 @@
+﻿# To run:
+# ./snap-build.ps1
+#
+# and then push to snap with:
+# cd ../dist/snap
+# snap push bw*.snap
+
+$dir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$rootDir = $dir + "/.."
+$distDir = $rootDir + "/dist"
+$snapDir = $rootDir + "/stores/snap"
+$distSnapDir = $distDir + "/snap"
+$snapYaml = $snapDir + "/snapcraft.yaml"
+$snapDistYaml = $distSnapDir + "/snapcraft.yaml"
+$srcPackage = $rootDir + "/package.json"
+$srcPackageVersion = (Get-Content -Raw -Path $srcPackage | ConvertFrom-Json).version
+
+if(Test-Path -Path $distSnapDir) {
+  Remove-Item -Recurse -Force $distSnapDir
+}
+
+Copy-Item -Path $snapDir -Destination $distSnapDir –Recurse
+(Get-Content $snapDistYaml).replace('__version__', $srcPackageVersion) | Set-Content $snapDistYaml
+cd $distSnapDir
+snapcraft
