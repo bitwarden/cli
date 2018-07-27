f9527383da34dfd7a9d21f800f3d2088c3d3d5bc
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-07-27 09:45:20 -0400

    brew update script

---
+++ scripts/brew-update.ps1
@@ -0,0 +1,13 @@
+﻿param (
+    [Parameter(Mandatory=$true)]
+    [string] $version
+)
+
+# Dependencies:
+# 1. brew cask install powershell
+#
+# To run:
+# pwsh ./brew-update.ps1 -version 1.1.0
+
+$url = 'https://registry.npmjs.org/@bitwarden/cli/-/cli-' + $version + '.tgz';
+brew bump-formula-pr --url $url bitwarden-cli
