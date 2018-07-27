56395ede458854538a2105c91fa94417fcfc89ee
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-07-27 09:22:55 -0400

    allow version param

--- scripts/snap-build.ps1
+++ scripts/snap-build.ps1
@@ -1,8 +1,13 @@
-﻿# Dependencies:
+﻿param (
+    [Parameter(Mandatory=$true)]
+    [string] $version
+)
+
+# Dependencies:
 # 1. Install powershell, ex `sudo apt-get install -y powershell`
 #
 # To run:
-# ./snap-build.ps1
+# ./snap-build.ps1 -version 1.1.0
 #
 # and then push to snap with:
 # cd ../dist/snap
@@ -15,14 +20,12 @@ $distDir = $rootDir + "/dist"
 $snapDir = $rootDir + "/stores/snap"
 $distSnapDir = $distDir + "/snap"
 $snapDistYaml = $distSnapDir + "/snapcraft.yaml"
-$srcPackage = $rootDir + "/package.json"
-$srcPackageVersion = (Get-Content -Raw -Path $srcPackage | ConvertFrom-Json).version
 
 if(Test-Path -Path $distSnapDir) {
   Remove-Item -Recurse -Force $distSnapDir
 }
 
 Copy-Item -Path $snapDir -Destination $distSnapDir –Recurse
-(Get-Content $snapDistYaml).replace('__version__', $srcPackageVersion) | Set-Content $snapDistYaml
+(Get-Content $snapDistYaml).replace('__version__', $version) | Set-Content $snapDistYaml
 cd $distSnapDir
 snapcraft
