3c2c1e69c5d1119e42d78c71536bfe228fb4e8e6
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-02-18 22:36:04 -0500

    bitwarden inc.

--- scripts/make-versioninfo.ps1
+++ scripts/make-versioninfo.ps1
@@ -12,14 +12,14 @@ BLOCK "StringFileInfo"
 {
 	BLOCK "040904b0"
 	{
-		VALUE "CompanyName", "8bit Solutions LLC"
+		VALUE "CompanyName", "Bitwarden Inc."
 		VALUE "ProductName", "Bitwarden"
 		VALUE "FileDescription", "Bitwarden CLI"
 		VALUE "FileVersion", "$env:PACKAGE_VERSION"
 		VALUE "ProductVersion", "$env:PACKAGE_VERSION"
 		VALUE "OriginalFilename", "bw.exe"
 		VALUE "InternalName", "bw"
-		VALUE "LegalCopyright", "Copyright 8bit Solutions LLC"
+		VALUE "LegalCopyright", "Copyright Bitwarden Inc."
 	}
 }
 
