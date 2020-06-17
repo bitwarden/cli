11e3bd99ed74e25f9b35553d8b05aed907ad714b
Kyle Spearrin <kyle.spearrin@gmail.com>
2020-06-16 20:56:11 -0400

    add hidePasswords functionality

--- src/models/selectionReadOnly.ts
+++ src/models/selectionReadOnly.ts
@@ -1,13 +1,15 @@
 export class SelectionReadOnly {
     static template(): SelectionReadOnly {
-        return new SelectionReadOnly('00000000-0000-0000-0000-000000000000', false);
+        return new SelectionReadOnly('00000000-0000-0000-0000-000000000000', false, false);
     }
 
     id: string;
     readOnly: boolean;
+    hidePasswords: boolean;
 
-    constructor(id: string, readOnly: boolean) {
+    constructor(id: string, readOnly: boolean, hidePasswords: boolean) {
         this.id = id;
         this.readOnly = readOnly;
+        this.hidePasswords = hidePasswords || false;
     }
 }
