4e816b8c04ec18f60de0db27b0371cc44d02aeaa
Matt Gibson <mgibson@bitwarden.com>
2021-05-13 14:22:24 -0500

    Specify type of send from templateType

--- src/models/response/sendResponse.ts
+++ src/models/response/sendResponse.ts
@@ -18,7 +18,7 @@ export class SendResponse implements BaseResponse {
         const req = new SendResponse();
         req.name = 'Send name';
         req.notes = 'Some notes about this send.';
-        req.type = SendType.Text;
+        req.type = sendType === SendType.Text ? SendType.Text : SendType.File;
         req.text = sendType === SendType.Text ? SendTextResponse.template() : null;
         req.file = sendType === SendType.File ? SendFileResponse.template() : null;
         req.maxAccessCount = null;
