b25b882db7680c6a3355d0f49e1d420e9cca8f9d
Matt Gibson <mgibson@bitwarden.com>
2021-06-19 10:02:48 -0400

    Use cryptoservice to compare key to local keyhash

--- src/commands/unlock.command.ts
+++ src/commands/unlock.command.ts
@@ -36,14 +36,12 @@ export class UnlockCommand {
         const kdf = await this.userService.getKdf();
         const kdfIterations = await this.userService.getKdfIterations();
         const key = await this.cryptoService.makeKey(password, email, kdf, kdfIterations);
-        const keyHash = await this.cryptoService.hashPassword(password, key);
 
         let passwordValid = false;
-        if (keyHash != null) {
-            const storedKeyHash = await this.cryptoService.getKeyHash();
-            if (storedKeyHash != null) {
-                passwordValid = storedKeyHash === keyHash;
-            } else {
+        if (key != null) {
+            passwordValid = await this.cryptoService.compareAndUpdateKeyHash(password, key);
+            if (!passwordValid) {
+                const keyHash = await this.cryptoService.hashPassword(password, key);
                 const request = new PasswordVerificationRequest();
                 request.masterPasswordHash = keyHash;
                 try {
