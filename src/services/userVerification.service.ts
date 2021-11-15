b25c7ce47cb9f412577458adb9dbf8a5ac104e40
Thomas Rittson <trittson@bitwarden.com>
2021-11-16 08:50:53 +1000

    Delete unneeded subclass

--- src/services/userVerification.service.ts
+++
@@ -1,7 +0,0 @@
-import { UserVerificationService as BaseUserVerificationService } from 'jslib-common/services/userVerification.service';
-
-export class UserVerificationService extends BaseUserVerificationService {
-    handleError(message: string) {
-        throw new Error(message);
-    }
-}
