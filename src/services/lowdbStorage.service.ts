be59e3af590cad3902ce2026dcf0e8c5e7013f79
Matt Gibson <mgibson@bitwarden.com>
2022-02-09 18:21:12 -0500

    Increase retry frequency and attempt count for lock file

--- src/services/lowdbStorage.service.ts
+++ src/services/lowdbStorage.service.ts
@@ -1,10 +1,18 @@
 import * as lock from "proper-lockfile";
+import { OperationOptions } from "retry";
 
 import { LogService } from "jslib-common/abstractions/log.service";
 
 import { LowdbStorageService as LowdbStorageServiceBase } from "jslib-node/services/lowdbStorage.service";
 
 import { Utils } from "jslib-common/misc/utils";
+
+const retries: OperationOptions = {
+  retries: 10,
+  minTimeout: 50,
+  maxTimeout: 1000,
+  factor: 1.4,
+};
 
 export class LowdbStorageService extends LowdbStorageServiceBase {
   constructor(
@@ -20,7 +28,7 @@ export class LowdbStorageService extends LowdbStorageServiceBase {
   protected async lockDbFile<T>(action: () => T): Promise<T> {
     if (this.requireLock && !Utils.isNullOrWhitespace(this.dataFilePath)) {
       this.logService.info("acquiring db file lock");
-      return await lock.lock(this.dataFilePath, { retries: 3 }).then((release) => {
+      return await lock.lock(this.dataFilePath, { retries: retries }).then((release) => {
         try {
           return action();
         } finally {
