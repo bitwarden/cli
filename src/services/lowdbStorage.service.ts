ee664059d277448dd8f77ce4073d559a8e69c9ee
Matt Gibson <mgibson@bitwarden.com>
2022-02-10 10:24:41 -0600

    Fix/lock lowdb file (#470)
    * Lock data.json while running
    
    * Await floating promises
    
    * Increase retry frequency and attempt count for lock file
    
    * tweak lock retry times

---
+++ src/services/lowdbStorage.service.ts
@@ -0,0 +1,42 @@
+import * as lock from "proper-lockfile";
+import { OperationOptions } from "retry";
+
+import { LogService } from "jslib-common/abstractions/log.service";
+
+import { LowdbStorageService as LowdbStorageServiceBase } from "jslib-node/services/lowdbStorage.service";
+
+import { Utils } from "jslib-common/misc/utils";
+
+const retries: OperationOptions = {
+  retries: 50,
+  minTimeout: 100,
+  maxTimeout: 250,
+  factor: 2,
+};
+
+export class LowdbStorageService extends LowdbStorageServiceBase {
+  constructor(
+    logService: LogService,
+    defaults?: any,
+    dir?: string,
+    allowCache = false,
+    private requireLock = false
+  ) {
+    super(logService, defaults, dir, allowCache);
+  }
+
+  protected async lockDbFile<T>(action: () => T): Promise<T> {
+    if (this.requireLock && !Utils.isNullOrWhitespace(this.dataFilePath)) {
+      this.logService.info("acquiring db file lock");
+      return await lock.lock(this.dataFilePath, { retries: retries }).then((release) => {
+        try {
+          return action();
+        } finally {
+          release();
+        }
+      });
+    } else {
+      return action();
+    }
+  }
+}
